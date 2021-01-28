fe8fa0f1dabcf3e5eaa1fb3838db14f0e8c2ab1d
Helge Spieker <helge@simula.no>
2021-01-28 16:13:34 +0100

    Update to Spring Boot 2.1.1 & Java 11

--- acumos-portal-fe/src/main/java/org/acumos/portal/fe/ApplicationWebXml.java
+++ acumos-portal-fe/src/main/java/org/acumos/portal/fe/ApplicationWebXml.java
@@ -21,7 +21,7 @@
 package org.acumos.portal.fe;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ApplicationWebXml extends SpringBootServletInitializer {
 
